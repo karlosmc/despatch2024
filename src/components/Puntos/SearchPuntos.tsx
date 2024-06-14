2a7edf15402e08c455c4bb2a462f76aff2923dc2
karlosmc <k4rlos.mc@gmail.com>
2024-06-14 11:16:15 -0500

    Antes de despliegue de prueba

---
+++ src/components/Puntos/SearchPuntos.tsx
@@ -0,0 +1,182 @@
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Icon, Fab, FormControl, MenuItem, InputLabel, Box, SelectChangeEvent, Select, Button, TextField, TablePagination, CircularProgress } from '@mui/material'
+import React, { useState } from 'react'
+import clienteAxios from '../../config/axios';
+import { Producto } from '../../types/producto.interface';
+
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import GradeIcon from '@mui/icons-material/Grade';
+
+import StoreIcon from '@mui/icons-material/Store';
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+
+
+import PanToolAltIcon from '@mui/icons-material/PanToolAlt';
+import { puntoUbicacion } from '../../types/puntoubicacion.interface';
+import useSWR from 'swr';
+
+
+interface SearchProductoProps {
+  onCheck: (puntos: puntoUbicacion) => void;
+}
+
+const SearchPuntos = ({ onCheck }: SearchProductoProps) => {
+
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  const [foundPuntos, setfoundPuntos] = useState<puntoUbicacion[]>([])
+
+  const [searchField, setSearchField] = useState('')
+
+  const [inputQuery, setInputQuery] = useState('')
+
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchField(event.target.value as string);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputQuery(event.target.value as string);
+  }
+
+  const handleSearch = async () => {
+    try {
+      setIsLoading(true)
+      const { data, status } = await clienteAxios(`/api/puntos/buscar?${searchField}=${inputQuery}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      if (status === 200) {
+        setfoundPuntos(data?.data)
+        setIsLoading(false)
+      }
+      // console.log(data)
+
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleCheck = (puntos: puntoUbicacion) => {
+    onCheck(puntos)
+  }
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const rows = [];
+
+  foundPuntos?.forEach((fil:puntoUbicacion) => {
+    rows.push(
+      <TableRow
+        key={fil.id}
+      >
+        <TableCell align="left">{fil.id}</TableCell>
+        <TableCell align="left">{fil.ruc}</TableCell>
+        <TableCell align="left">{fil.codLocal}</TableCell>
+        <TableCell align="left">{fil.direccion}</TableCell>
+        <TableCell align="left">{fil.fullubigeo}</TableCell>
+        
+        <TableCell align="left"><Icon color='warning' >{fil.fav ? <GradeIcon /> : <StarOutlineIcon />}</Icon></TableCell>
+        <TableCell align="left"><Icon color={fil.isCompany?'info':'action'} >{fil.isCompany ? <StoreIcon /> : <StoreOutlinedIcon />}</Icon></TableCell>
+        {/* <TableCell align="left"><Fab color='primary' size='small' onClick={() => handleEditPunto(fil.id)} ><EditIcon /></Fab></TableCell> */}
+        <TableCell align="center"><Fab color='success' size='small' onClick={()=>handleCheck(fil)}><PanToolAltIcon sx={{color:'white'}} /></Fab></TableCell>
+      </TableRow>
+    )
+  })
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <>
+      <Box display={'flex'} flexDirection={'row'}>
+        <FormControl fullWidth margin='normal' size='small'>
+          <InputLabel id="demo-simple-select-label">Búsqueda por:</InputLabel>
+          <Select
+            name='codigo'
+            value={searchField}
+            label="Búsqueda por"
+            onChange={handleChange}
+          >
+            <MenuItem value={'ruc'}>RUC</MenuItem>
+            <MenuItem value={'ubigeo'}>UBIGEO</MenuItem>
+            <MenuItem value={'direccion'}>Dirección</MenuItem>
+            <MenuItem value={'fullubigeo'}>Ubigeo completo</MenuItem>
+            <MenuItem value={'nombreCorto'}>Nombre corto</MenuItem>
+            
+          </Select>
+        </FormControl>
+
+      </Box>
+      <TextField
+        value={inputQuery}
+        onChange={handleInputChange}
+        label='Texto a buscar'
+        margin='normal'
+        name='descripcion'
+        size='small'
+        fullWidth
+        sx={{ mb: 1 }}
+
+      />
+      <Button fullWidth sx={{ my: 2 }} size='small' variant='contained' color='primary' onClick={handleSearch}>
+        Buscar
+      </Button>
+
+      <TableContainer component={Paper} >
+        <Table aria-label="simple table" size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell width={'5%'}>Id</TableCell>
+              <TableCell width={'10%'} align="left">Ruc</TableCell>
+              <TableCell width={'5%'} align="left">Cod. Local</TableCell>
+              <TableCell width={'40%'} align="left">Dirección</TableCell>
+              <TableCell width={'20%'} align="left">Ubigeo</TableCell>
+              <TableCell width={'10%'} align="left">Fav?</TableCell>
+              <TableCell width={'10%'} align="left">Propio?</TableCell>
+              <TableCell width={'10%'} align="left">Seleccionar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              isLoading ? (<TableRow style={{ height: 53 * rowsPerPage }}><TableCell colSpan={11} align="center"><CircularProgress /></TableCell></TableRow>) :
+                rows.length > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  :
+                  <TableRow style={{ height: 53 * rowsPerPage }}><TableCell colSpan={11} align="center"><h2>No hay resultados</h2></TableCell></TableRow>
+            }
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}><TableCell colSpan={11} /></TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  )
+}
+
+export default SearchPuntos
--- src/pages/Direccion/index.tsx
+++ src/pages/Direccion/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, MouseEvent } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import { Stack, Button, Chip, Paper, InputAdornment, Tooltip, withStyles, Theme, styled, TooltipProps, tooltipClasses, keyframes } from "@mui/material";
-import { Departamentos, Provincias, Ubigeos } from "../../types/ubigeo";
+import { Stack, Button, Chip, Paper, InputAdornment, Tooltip, styled, TooltipProps, tooltipClasses, keyframes, Box } from "@mui/material";
+
 import { useFormik } from "formik";
 
 import * as Yup from "yup";
@@ -10,13 +10,18 @@ import { DirectionsUser, UserLogin } from "../../types/login.interface";
 import { Direccion } from "../../types/guias/guiaremision.interface";
 import { PartidaSchema } from "../../utils/validateGuiaRemision";
 import InfoIcon from '@mui/icons-material/Info';
+import { Ubigeos } from "../../types/ubigeos.interface";
+import { DialogComponentCustom } from "../../components";
+import ModalPuntoUbicacion from "../../components/Puntos";
+import { puntoUbicacion } from "../../types/puntoubicacion.interface";
+import SearchPuntos from "../../components/Puntos/SearchPuntos";
 
 
 interface DireccionFormProps {
   initialValue: Direccion;
   onChange: (direccion: Direccion) => void;
   schema?: Yup.AnyObjectSchema;
-  codTraslado:string;
+  codTraslado: string;
 }
 
 const DireccionValues: Direccion = {
@@ -59,12 +64,12 @@ const pulse = keyframes`
 `;
 
 // Creamos un componente estilizado para el InputAdornment
-const AnimatedInputAdornment = styled(InputAdornment)(({}) => ({
+const AnimatedInputAdornment = styled(InputAdornment)(({ }) => ({
   animation: `${pulse} 2s infinite`,
   display: 'flex',
   alignItems: 'center',
-  height:'auto',
-  borderRadius:'50%'
+  height: 'auto',
+  borderRadius: '50%'
 }));
 
 const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
@@ -78,14 +83,21 @@ const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
   },
 }));
 
+type ModalsProps = {
+  open: boolean;
+  form: React.ReactNode | null;
+  title: string;
+};
+
+
 const DatosDireccion = ({
   initialValue,
   onChange,
   schema,
   codTraslado
 }: DireccionFormProps) => {
-  if (codTraslado!=='04'){
-    initialValue.codLocal='0000'
+  if (codTraslado !== '04') {
+    initialValue.codLocal = '0000'
   }
   const formik = useFormik({
     initialValues: initialValue,
@@ -99,6 +111,21 @@ const DatosDireccion = ({
 
   const [ubigeoInicial, setUbigeoInicial] = useState<string>(initialValue.ubigeo || '')
 
+  const [inputValue, setInputValue] = useState('');
+  const [selectedUbigeo, setSelectedUbigeo] = useState<string | null>(null);
+
+  const [value, setValue] = useState<Ubigeos | null>(null);
+
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  const [modalsForm, setModalsForms] = useState<ModalsProps>({
+    open: false,
+    form: null,
+    title: "",
+  });
+
+
+
   // const dataUser: UserLogin = JSON.parse(localStorage.getItem('userlogin'));
   // console.log(dataUser);
 
@@ -109,121 +136,21 @@ const DatosDireccion = ({
   const listDirections: DirectionsUser[] = [];
   // const correlativo = parseInt(dataUser.sercor.correlativo)+1
 
-  const [departamentos, setDepartamentos] = useState<Departamentos[]>([]);
-  const [provincias, setProvincias] = useState<Provincias[]>([]);
+
   const [ubigeos, setUbigeos] = useState<Ubigeos[]>([]);
 
-  const [filteredProvincias, setFilteredProvincias] = useState<Provincias[]>(
-    []
-  );
 
-  const [filteredUbigeos, setFilteredUbigeos] = useState<Ubigeos[]>([]);
-
-  const [selectedDepartamento, setSelectedDepartamento] =
-    useState<Departamentos | null>(null);
-  const [selectedProvincia, setSelectedProvincia] = useState<Provincias | null>(
-    null
-  );
-  const [selectedUbigeo, setSelectedUbigeo] = useState<Ubigeos | null>(null);
 
 
   useEffect(() => {
     const loadData = async () => {
-      const loadDepartamento = await fetch("src/files/Departamentos.json");
-      const loadDepartamentoJson = await loadDepartamento.json();
-      setDepartamentos(loadDepartamentoJson);
-
-      const loadProvincia = await fetch("src/files/Provincias.json");
-      const loadProvinciaJson = await loadProvincia.json();
-      setProvincias(loadProvinciaJson);
-
-      const loadUbigeo = await fetch("src/files/ubigeo.json");
+      const loadUbigeo = await fetch("src/files/ubigeos.json");
       const loadUbigeoJson = await loadUbigeo.json();
       setUbigeos(loadUbigeoJson);
     };
 
     loadData();
   }, []);
-
-  /* Filtrar provincias según el departamento seleccionado */
-  useEffect(() => {
-    // Filtrar provincias según el departamento seleccionado
-    if (selectedDepartamento) {
-      const filtered = provincias.filter(
-        (provincia) =>
-          provincia.Codigo.substring(0, 2) === selectedDepartamento.Codigo
-      );
-      setFilteredProvincias(filtered);
-      setSelectedProvincia(null);
-      setSelectedUbigeo(null);
-    } else {
-      setFilteredProvincias([]);
-      setFilteredUbigeos([]);
-      setSelectedProvincia(null);
-      setSelectedUbigeo(null);
-      if (ubigeoInicial.length === 0) {
-        setSelectedUbigeo(null);
-      }
-    }
-  }, [selectedDepartamento]);
-
-  /* Filtrar ubigeos según la provincia seleccionada */
-  useEffect(() => {
-    // Filtrar ubigeos según la provincia seleccionada
-    if (selectedProvincia) {
-      const filtered = ubigeos.filter((ubigeo) =>
-        ubigeo.Codigo.startsWith(selectedProvincia.Codigo.substring(0, 4))
-      );
-      setFilteredUbigeos(filtered);
-
-      if (ubigeoInicial.length === 0) {
-        setSelectedUbigeo(null);
-      }
-    } else {
-      setFilteredUbigeos([]);
-      setSelectedUbigeo(null);
-    }
-  }, [selectedProvincia]);
-
-
-  /*  Carga inicial utilizando el código de ubigeo proporcionado por el componente padre */
-  useEffect(() => {
-    // Carga inicial utilizando el código de ubigeo proporcionado por el componente padre
-    if (
-      ubigeoInicial.length > 0 &&
-      provincias.length > 0 &&
-      ubigeos.length > 0
-    ) {
-      const initialUbigeo = ubigeos.find(
-        (ubigeo) => ubigeo.Codigo === ubigeoInicial
-      );
-
-      if (initialUbigeo) {
-        setSelectedUbigeo(initialUbigeo);
-        const provinciaCodigo = initialUbigeo.Codigo.substring(0, 4);
-        const departamentoCodigo = provinciaCodigo.substring(0, 2);
-        const departamento = departamentos.find(
-          (dep) => dep.Codigo === departamentoCodigo
-        );
-        if (departamento) {
-          setSelectedDepartamento(departamento);
-          const provinciasFiltradas = provincias.filter((prov) =>
-            prov.Codigo.startsWith(departamento.Codigo.substring(0, 2))
-          );
-          const provincia = provinciasFiltradas.find(
-            (prov) => prov.Codigo === provinciaCodigo
-          );
-          if (provincia) {
-            setFilteredProvincias(provinciasFiltradas);
-            setSelectedProvincia(provincia);
-
-          }
-        }
-      }
-    }
-  }, [ubigeoInicial, ubigeos, departamentos, provincias, filteredUbigeos]);
-
-  // }, [ubigeoInicial, ubigeos, departamentos, provincias,filteredUbigeos]);
 
   /* establecer valores dando click a los valores por defecto */
   useEffect(() => {
@@ -239,30 +166,28 @@ const DatosDireccion = ({
   }, [directionChip])
 
 
+  const handleOpenModalForm = (form: React.ReactNode, title: string) => {
+    setModalsForms({ open: true, form, title });
+  };
 
-  useEffect(() => {
-    // Filtrar ubigeos nuevamente cuando se cambia el departamento o provincia seleccionada
-    if (selectedDepartamento && selectedProvincia) {
-      const filtered = ubigeos.filter((ubigeo) =>
-        ubigeo.Codigo.startsWith(selectedProvincia.Codigo.substring(0, 4))
-      );
-      setFilteredUbigeos(filtered);
-    }
-  }, [selectedDepartamento, selectedProvincia]);
-
-
+  const handleCloseModalForm = () => {
+    // Cierra el modal en la posición especificada
+    setModalsForms((prev) => ({ ...prev, open: false }));
+  };
 
   const handleClean = () => {
     // setDataDireccion(DireccionValues);
     onChange(DireccionValues);
   };
 
-  const handleUbigeoChange = (evt: any, newValue: Ubigeos | null) => {
-    evt.preventDefault();
-    if (newValue) {
-      setSelectedUbigeo(newValue);
-      formik.setFieldValue("ubigeo", newValue.Codigo);
-    };
+
+  const handleConfirm=(punto:puntoUbicacion):void=>{
+
+    formik.setFieldValue('ubigeo', punto.ubigeo)
+    formik.setFieldValue('direccion', punto.direccion)
+    formik.setFieldValue('codLocal', punto.codLocal)
+    formik.setFieldValue('ruc', punto.ruc?punto.ruc:'')
+    handleCloseModalForm()
   }
 
   const handleClickDirection = (evt: MouseEvent<HTMLDivElement>) => {
@@ -271,16 +196,39 @@ const DatosDireccion = ({
     setDirectionChip(listDirections.find(item => item.id === spanChip));
   };
 
-  const handleOnInputChange = (
-    _evt: React.ChangeEvent<{}>,
-    _newValue: string,
-    reason: string
-  ) => {
-    if (reason === "clear") {
-      // clearUbigeo();
+  const onChangeUbigeo = (_event: unknown, newValue: Ubigeos) => {
+    if (newValue) {
+      setValue(newValue)
+      setSelectedUbigeo(newValue.ubigeo);
+      formik.setFieldValue('ubigeo', newValue.ubigeo)
+    } else {
       setSelectedUbigeo(null);
+      formik.setFieldValue('ubigeo', '')
     }
-  };
+  }
+
+  // const handleCheckProduct = (punto:puntoUbicacion):void => {
+
+  //   formik.setFieldValue('ubi', producto.ubi)
+  //   formik.setFieldValue('codProdSunat', producto.codProdSunat)
+  //   formik.setFieldValue('descripcion', producto.descripcion)
+  //   formik.setFieldValue('unidad', producto.unidad)
+  //   handleCloseModalForm()
+  // }
+
+
+  useEffect(() => {
+
+    if (ubigeos.length > 0) {
+      const initialUbigeo = ubigeos.find(option => option.ubigeo === formik.values?.ubigeo) || null;
+      // console.log(initialUbigeo)
+      setValue(initialUbigeo);
+      setSelectedUbigeo(initialUbigeo?.ubigeo || null);
+    }
+  }, [formik.values.ubigeo,ubigeos]);
+
+
+
 
   return (
     <>
@@ -323,7 +271,30 @@ const DatosDireccion = ({
           );
         })}
       </Paper>
-      <form onSubmit={formik.handleSubmit}>
+      <Box display={'flex'} flexDirection={'row'} gap={2} my={2}>
+        <Button fullWidth variant="contained" color="success"
+          onClick={() => {
+            handleOpenModalForm(
+              <ModalPuntoUbicacion initialValue={null} edit={false} onConfirm={handleConfirm} />,
+              'Punto Ubicacion'
+            )
+          }}
+        >
+          Crear
+        </Button>
+
+        <Button fullWidth variant="contained" color="warning"
+         onClick={() => {
+          handleOpenModalForm(
+            <SearchPuntos onCheck={handleConfirm}/>,
+            'Buscar Punto de ubicación'
+          )
+        }}
+        >
+          Buscar
+        </Button>
+      </Box>
+      <Box component={'form'} onSubmit={formik.handleSubmit}>
         <TextField
           margin="normal"
           size="small"
@@ -331,7 +302,7 @@ const DatosDireccion = ({
           name="codLocal"
           type="text"
           label="Código de Local"
-          sx={{ my:1.5 }}
+          sx={{ my: 1.5 }}
           value={formik.values.codLocal}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
@@ -341,7 +312,7 @@ const DatosDireccion = ({
             endAdornment: (
               <AnimatedInputAdornment position="end">
                 <LightTooltip arrow title="Usa Código de Local solo si vas a considerar un RUC para establecer el punto de Ubicación, si no, dejalo por defecto {0000}">
-                  <InfoIcon color="action"/>
+                  <InfoIcon color="action" />
                 </LightTooltip>
               </AnimatedInputAdornment>
             ),
@@ -354,8 +325,8 @@ const DatosDireccion = ({
           name="direccion"
           type="text"
           label="Dirección"
-          sx={{ my:1.5 }}
-       
+          sx={{ my: 1.5 }}
+
           value={formik.values.direccion}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
@@ -369,8 +340,8 @@ const DatosDireccion = ({
           name="ruc"
           type="text"
           label="R.U.C. (solo valido para RUC de 11 caracteres)"
-          sx={{ my:1.5 }}
-        
+          sx={{ my: 1.5 }}
+
           value={formik.values.ruc}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
@@ -380,78 +351,38 @@ const DatosDireccion = ({
             endAdornment: (
               <AnimatedInputAdornment position="end">
                 <LightTooltip arrow title="No uses un R.U.C. si vas a consignar un DNI, dejalo en blanco. SUNAT hará la validación y va a notificar un ERROR">
-                  <InfoIcon color="action"/>
+                  <InfoIcon color="action" />
                 </LightTooltip>
               </AnimatedInputAdornment>
             ),
           }}
         />
         <Autocomplete
-          options={departamentos}
-          getOptionLabel={(departamento) => departamento.Departamento}
-          value={selectedDepartamento || null}
-          onInputChange={handleOnInputChange}
-          onChange={(_, value) => setSelectedDepartamento(value)}
-          size="small"
+          size='small'
+          options={ubigeos}
+          value={value}
+          getOptionLabel={(option) => option.fullubigeo}
+          filterOptions={(options, state) =>
+            options.filter((option) =>
+              option.distrito.toLowerCase().includes(state.inputValue.toLowerCase())
+            )
+          }
           renderInput={(params) => (
-            <TextField margin="normal" {...params} label="Departamento" />
+            <TextField
+              {...params}
+              label="Selecciona un Ubigeo"
+              variant="outlined"
+            // onChange={(event) => setInputValue(event.target.value)}
+            />
           )}
-          isOptionEqualToValue={(option, value) =>
-            option.Codigo === value.Codigo
-          }
+          inputValue={inputValue}
+          onInputChange={(_event, newInputValue) => setInputValue(newInputValue)}
+          onChange={onChangeUbigeo}
         />
 
-        <Autocomplete
-          options={filteredProvincias}
-          getOptionLabel={(provincia) => provincia.Provincia}
-          value={selectedProvincia || null}
-          onInputChange={handleOnInputChange}
-          onChange={(_, value) => setSelectedProvincia(value)}
-          size="small"
-          renderInput={(params) => <TextField margin="normal" {...params} label="Provincia" />}
-          disabled={!selectedDepartamento} // Deshabilitar si no hay departamento seleccionado
-          isOptionEqualToValue={(option, value) =>
-            option.Codigo === value.Codigo
-          }
-        />
 
-        <Autocomplete
-          options={filteredUbigeos}
-          getOptionLabel={(ubigeo) => ubigeo.Ubigeo}
-          value={selectedUbigeo || null}
-          onChange={handleUbigeoChange}
-          size="small"
-          
-          onInputChange={handleOnInputChange}
-          //onChange={(_, value) => setSelectedUbigeo(value)}
-          renderInput={(params) => <TextField margin="normal" {...params} label="Distrito" />}
-          //disabled={!selectedProvincia} // Deshabilitar si no hay provincia seleccionada
-          disabled={!selectedProvincia || !filteredProvincias.length}
-          isOptionEqualToValue={(option, value) =>
-            option.Codigo === value.Codigo
-          }
-        />
-        <TextField
-          required
-          margin="normal"
-          inputProps={{
-            readOnly: true,
-          }}
-          size="small"
-          fullWidth
-          name="ubigeo"
-          type="text"
-          label="Ubigeo"
-          sx={{ my:1.5 }}
-       
 
-          value={formik.values.ubigeo}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          helperText={formik.touched.ubigeo && formik.errors.ubigeo}
-          error={formik.touched.ubigeo && Boolean(formik.errors.ubigeo)}
-        />
-        <Stack direction="row" spacing={2}>
+        <Stack direction="row" spacing={2} mt={1}>
           <Button variant="outlined" color="success" type="submit">
             Agregar
           </Button>
@@ -460,7 +391,22 @@ const DatosDireccion = ({
             Clean
           </Button>
         </Stack>
-      </form>
+      </Box>
+      <DialogComponentCustom
+        closeButton={
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => handleCloseModalForm()}
+          >
+            Cerrar
+          </Button>
+        }
+        open={modalsForm.open}
+        title={modalsForm.title}
+        element={modalsForm.form}
+
+      />
     </>
   );
 };
